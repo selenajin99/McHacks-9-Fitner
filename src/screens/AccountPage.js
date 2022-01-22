5f35cfc04b776af5182ef7d292152412b4ab7c95
Saagar <saagarkarya@gmail.com>
2022-01-21 23:30:51 -0500

    Git ignore again
    https://stackoverflow.com/questions/49099131/recommended-gitignore-for-react-native

--- src/screens/AccountPage.js
+++ src/screens/AccountPage.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState } from 'react'
+import { View, Text } from 'react-native'
+import firestore from '@react-native-firebase/firestore'
+import { CheckBox } from '@ui-kitten/components'
 
 const AccountPage = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
-};
+	const [monday, setMonday] = useState([0, 1, 2])
+	const [mondayChecked, setMondayChecked] = useState
+	const refreshMonday = () => {}
+	return (
+		<View>
+			<Text></Text>
+			<CheckBox
+				checked={monday[0] && monday[1] && monday[2]}
+				// indeterminate={monday.morning || monday.afternoon || monday.evening}
+			>
+				Monday
+			</CheckBox>
+			{/* <CheckBox
+				checked={() => {
+					if (monday.morning) {
+						return true
+					}
+				}}
+				onChange={() => {
+					console.log(monday)
+					setMonday({ ...monday, morning: !monday.morning })
+				}}>
+				Morning
+			</CheckBox> */}
+		</View>
+	)
+}
 
-export default AccountPage;
+export default AccountPage
