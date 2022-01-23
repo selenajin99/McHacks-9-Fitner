4836565f61cfe86eede0ab045178fe7d53d8551a
Kevin Nam <dong.g.nam@mail.mcgill.ca>
2022-01-23 03:17:27 -0500

    fixed profile page scroll issue

--- src/screens/ProfilePage.js
+++ src/screens/ProfilePage.js
@@ -1,206 +1,260 @@
 import React from 'react';
-import {Text, Avatar} from '@ui-kitten/components';
-import {View, StyleSheet, ScrollView, Dimensions} from 'react-native';
+import {Avatar, Card} from '@ui-kitten/components';
+import {Text, View, StyleSheet, ScrollView, Dimensions} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const ProfilePage = ({route}) => {
   return (
-    <ScrollView nestedScrollEnabled={true}>
-      <Avatar
-        style={styles.profile}
-        source={{
-          uri: route.params.user.imageUri,
-        }}
-      />
-      <Text style={styles.name}>{route.params.user.Name}</Text>
-      <Text style={styles.categories}>Bio</Text>
-      <View style={styles.bio}>
+    <ScrollView
+      nestedScrollEnabled={true}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}>
+      <View style={{flex: 1}}>
+        <View style={{alignItems: 'center'}}>
+          <Avatar
+            style={styles.profile}
+            source={{
+              uri: route.params.user.imageUri,
+            }}
+          />
+        </View>
+        <View>
+          <Text style={styles.name}>{route.params.user.Name}</Text>
+          <Text style={styles.categories}>Bio</Text>
+          <Card
+            style={{
+              marginHorizontal: 20,
+              borderRadius: 30,
+              height: 90,
+              borderStyle: 'dotted',
+            }}>
+            <Text>{route.params.user.bio}</Text>
+          </Card>
+        </View>
+        <View>
+          <Text style={styles.categories}>Activities</Text>
+          <View
+            style={{
+              flexDirection: 'row',
+              // flexWrap: 'wrap',
+              marginHorizontal: '5%',
+            }}>
+            {route.params.user.activities.map((activity, index) => {
+              return (
+                <View style={styles.chip}>
+                  <Text key={index}>{activity}</Text>
+                </View>
+              );
+            })}
+          </View>
+        </View>
         <ScrollView nestedScrollEnabled={true}>
-          <Text>{route.params.user.bio}</Text>
-        </ScrollView>
-      </View>
-      <Text style={styles.categories}>Activities</Text>
-      <View
-        style={{
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          marginHorizontal: '5%',
-        }}>
-        {route.params.user.activities.map((activity, index) => {
-          return (
-            <View style={styles.chip}>
-              <Text key={index}>{activity}</Text>
+          <Text style={styles.categories}>Availability</Text>
+          {/* availability on Monday */}
+          <View
+            style={{
+              // flex: 1,
+              // alignSelf: 'stretch',
+              flexDirection: 'row',
+              marginHorizontal: '10%',
+              marginVertical: '3%',
+            }}>
+            <View style={{flex: 4, alignSelf: 'stretch'}}>
+              <Text>MONDAY</Text>
             </View>
-          );
-        })}
-      </View>
-      <Text style={styles.categories}>Availability</Text>
-      {/* availability on Monday */}
-      <View
-        style={{
-          flex: 1,
-          alignSelf: 'stretch',
-          flexDirection: 'row',
-          marginHorizontal: '10%',
-          marginVertical: '3%',
-        }}>
-        <View style={{flex: 4, alignSelf: 'stretch'}}>
-          <Text>MONDAY</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Monday[0] ? '⏰' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Monday[1] ? '☀️' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Monday[2] ? '🌙' : ' '}</Text>
-        </View>
-      </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Monday[0] ? '⏰' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Monday[1] ? '☀️' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Monday[2] ? '🌙' : ' '}
+              </Text>
+            </View>
+          </View>
 
-      {/* availability on Tuesday */}
-      <View
-        style={{
-          flex: 1,
-          alignSelf: 'stretch',
-          flexDirection: 'row',
-          marginHorizontal: '10%',
-          marginVertical: '3%',
-        }}>
-        <View style={{flex: 4, alignSelf: 'stretch'}}>
-          <Text>TUESDAY</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Tuesday[0] ? '⏰' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Tuesday[1] ? '☀️' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Tuesday[2] ? '🌙' : ' '}</Text>
-        </View>
-      </View>
+          {/* availability on Tuesday */}
+          <View
+            style={{
+              // flex: 1,
+              // alignSelf: 'stretch',
+              flexDirection: 'row',
+              marginHorizontal: '10%',
+              marginVertical: '3%',
+            }}>
+            <View style={{flex: 4, alignSelf: 'stretch'}}>
+              <Text>TUESDAY</Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Tuesday[0] ? '⏰' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Tuesday[1] ? '☀️' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Tuesday[2] ? '🌙' : ' '}
+              </Text>
+            </View>
+          </View>
 
-      {/* availability on Wednesday */}
-      <View
-        style={{
-          flex: 1,
-          alignSelf: 'stretch',
-          flexDirection: 'row',
-          marginHorizontal: '10%',
-          marginVertical: '3%',
-        }}>
-        <View style={{flex: 4, alignSelf: 'stretch'}}>
-          <Text>WEDNESDAY</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>
-            {route.params.user.availability.Wednesday[0] ? '⏰' : ' '}
-          </Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>
-            {route.params.user.availability.Wednesday[1] ? '☀️' : ' '}
-          </Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>
-            {route.params.user.availability.Wednesday[2] ? '🌙' : ' '}
-          </Text>
-        </View>
-      </View>
+          {/* availability on Wednesday */}
+          <View
+            style={{
+              // flex: 1,
+              // alignSelf: 'stretch',
+              flexDirection: 'row',
+              marginHorizontal: '10%',
+              marginVertical: '3%',
+            }}>
+            <View style={{flex: 4, alignSelf: 'stretch'}}>
+              <Text>WEDNESDAY</Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Wednesday[0] ? '⏰' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Wednesday[1] ? '☀️' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Wednesday[2] ? '🌙' : ' '}
+              </Text>
+            </View>
+          </View>
 
-      {/* availability on Thursday */}
-      <View
-        style={{
-          flex: 1,
-          alignSelf: 'stretch',
-          flexDirection: 'row',
-          marginHorizontal: '10%',
-          marginVertical: '3%',
-        }}>
-        <View style={{flex: 4, alignSelf: 'stretch'}}>
-          <Text>THURSDAY</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Thursday[0] ? '⏰' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Thursday[1] ? '☀️' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Thursday[2] ? '🌙' : ' '}</Text>
-        </View>
-      </View>
+          {/* availability on Thursday */}
+          <View
+            style={{
+              // flex: 1,
+              // alignSelf: 'stretch',
+              flexDirection: 'row',
+              marginHorizontal: '10%',
+              marginVertical: '3%',
+            }}>
+            <View style={{flex: 4, alignSelf: 'stretch'}}>
+              <Text>THURSDAY</Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Thursday[0] ? '⏰' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Thursday[1] ? '☀️' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Thursday[2] ? '🌙' : ' '}
+              </Text>
+            </View>
+          </View>
 
-      {/* availability on Friday */}
-      <View
-        style={{
-          flex: 1,
-          alignSelf: 'stretch',
-          flexDirection: 'row',
-          marginHorizontal: '10%',
-          marginVertical: '3%',
-        }}>
-        <View style={{flex: 4, alignSelf: 'stretch'}}>
-          <Text>FRIDAY</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Friday[0] ? '⏰' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Friday[1] ? '☀️' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Friday[2] ? '🌙' : ' '}</Text>
-        </View>
-      </View>
+          {/* availability on Friday */}
+          <View
+            style={{
+              // flex: 1,
+              // alignSelf: 'stretch',
+              flexDirection: 'row',
+              marginHorizontal: '10%',
+              marginVertical: '3%',
+            }}>
+            <View style={{flex: 4, alignSelf: 'stretch'}}>
+              <Text>FRIDAY</Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Friday[0] ? '⏰' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Friday[1] ? '☀️' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Friday[2] ? '🌙' : ' '}
+              </Text>
+            </View>
+          </View>
 
-      {/* availability on Saturday */}
-      <View
-        style={{
-          flex: 1,
-          alignSelf: 'stretch',
-          flexDirection: 'row',
-          marginHorizontal: '10%',
-          marginVertical: '3%',
-        }}>
-        <View style={{flex: 4, alignSelf: 'stretch'}}>
-          <Text>SATURDAY</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Saturday[0] ? '⏰' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Saturday[1] ? '☀️' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Saturday[2] ? '🌙' : ' '}</Text>
-        </View>
-      </View>
+          {/* availability on Saturday */}
+          <View
+            style={{
+              // flex: 1,
+              // alignSelf: 'stretch',
+              flexDirection: 'row',
+              marginHorizontal: '10%',
+              marginVertical: '3%',
+            }}>
+            <View style={{flex: 4, alignSelf: 'stretch'}}>
+              <Text>SATURDAY</Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Saturday[0] ? '⏰' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Saturday[1] ? '☀️' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Saturday[2] ? '🌙' : ' '}
+              </Text>
+            </View>
+          </View>
 
-      {/* availability on Sunday */}
-      <View
-        style={{
-          flex: 1,
-          alignSelf: 'stretch',
-          flexDirection: 'row',
-          marginHorizontal: '10%',
-          marginVertical: '3%',
-        }}>
-        <View style={{flex: 4, alignSelf: 'stretch'}}>
-          <Text>SUNDAY</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Sunday[0] ? '⏰' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Sunday[1] ? '☀️' : ' '}</Text>
-        </View>
-        <View style={styles.tablecells}>
-          <Text>{route.params.user.availability.Sunday[2] ? '🌙' : ' '}</Text>
-        </View>
+          {/* availability on Sunday */}
+          <View
+            style={{
+              // flex: 1,
+              // alignSelf: 'stretch',
+              flexDirection: 'row',
+              marginHorizontal: '10%',
+              marginVertical: '3%',
+            }}>
+            <View style={{flex: 4, alignSelf: 'stretch'}}>
+              <Text>SUNDAY</Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Sunday[0] ? '⏰' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Sunday[1] ? '☀️' : ' '}
+              </Text>
+            </View>
+            <View style={styles.tablecells}>
+              <Text>
+                {route.params.user.availability.Sunday[2] ? '🌙' : ' '}
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     </ScrollView>
   );
@@ -217,27 +271,27 @@ const styles = StyleSheet.create({
     position: 'relative',
   },
   profile: {
-    top: 30,
-    width: Dimensions.get('window').width * 0.55,
-    height: Dimensions.get('window').width * 0.55,
-    alignSelf: 'center',
-    marginBottom: '5%',
+    // top: 30,
+    // width: Dimensions.get('window').width * 0.55,
+    // height: Dimensions.get('window').width * 0.55,
+    // alignSelf: 'center',
+    width: 200,
+    height: 200,
+    marginTop: 10,
   },
   right: {
     padding: 5,
-    position: 'relative',
-    flex: 1,
+    // position: 'relative',
+    // flex: 1,
   },
   name: {
     fontSize: 40,
-    marginTop: 20,
     marginHorizontal: '6%',
     alignSelf: 'flex-start',
   },
   categories: {
     fontSize: 20,
     marginBottom: 13,
-    marginTop: 20,
     marginHorizontal: '6%',
     alignSelf: 'flex-start',
   },
@@ -247,7 +301,7 @@ const styles = StyleSheet.create({
     borderStyle: 'dotted',
     borderRadius: 10,
     width: '90%',
-    height: '20%',
+    height: '30%',
     padding: '2%',
     marginHorizontal: '5%',
   },
@@ -265,8 +319,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
   tablecells: {
-    flex: 1.5,
-    alignSelf: 'stretch',
+    // flex: 1.5,
+    // alignSelf: 'stretch',
   },
   chat: {
     height: 32,
@@ -274,7 +328,7 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
     marginBottom: 10,
     marginLeft: 5,
-    border: 1,
+    // border: 1,
   },
 });
 
